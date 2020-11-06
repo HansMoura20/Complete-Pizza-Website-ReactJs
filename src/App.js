a2edbee425319f6067ff41f7977e4beb70aa83a2
HansMoura20 <hansadrienmoura@outlook.com>
2020-11-06 13:48:19 +0800

    Add Navbar, color and title styles

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
 import React from 'react';
+import { createGlobalStyle } from 'styled-components';
+import Navbar from './Navbar/Navbar';
 
+
+
+const GlobalStyle = createGlobalStyle`
+  body{
+    font-family: 'Open Sans', sans-serif;
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+  }
+
+  h1, h2, h3{
+    font-family: 'Righteous', cursive;
+  }
+
+`;
 
 function App() {
   return (
-    <div className="App">
-      Hello Sliceline
-    </div>
+    <>
+      <GlobalStyle/>
+      <Navbar/>
+      <div>Hello SliceLine</div>
+    </>
+ 
   );
 }
 
