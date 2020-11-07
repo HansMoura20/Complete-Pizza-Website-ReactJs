198587c696e422575cb79563b3f6f8c23adb3f31
HansMoura20 <hansadrienmoura@outlook.com>
2020-11-07 13:35:30 +0800

    Add Quantity input and hook

--- src/FoodDialog/FoodDialog.js
+++ src/FoodDialog/FoodDialog.js
@@ -4,6 +4,8 @@ import {FoodLabel} from "../Menu/FoodGrid";
 import { pizzaRed } from "../Styles/colors";
 import { Title } from "../Styles/title";
 import { formatPrice } from "../Data/FoodData";
+import { QuantityInput } from "./QuantityInput";
+import { useQuantity } from "../Hooks/useQuantity";
 
 const Dialog = styled.div`
   width: 500px;
@@ -26,6 +28,7 @@ const Dialog = styled.div`
 export const DialogContent = styled.div`
   overflow: auto;
   min-height: 100px;
+  padding: 0px 40px;
 `;
 
 export const DialogFooter = styled.div`
@@ -83,14 +86,22 @@ const DialogShadow = styled.div`
   }
   `;
 
-export function FoodDialog({openFood, setOpenFood, setOrders, orders}){
+export function getPrice(order){
+  return order.quantity * order.price;
+}
+
+function FoodDialogContainer({openFood, setOpenFood, setOrders, orders}){
+
+  const quantity = useQuantity(openFood && openFood.quantity);
+
   function close(){
     setOpenFood();
   }
-  if(!openFood) return null;
+  
 
   const order = {
-    ...openFood
+    ...openFood,
+    quantity: quantity.value
   }
 
   function addToOrder(){
@@ -106,14 +117,21 @@ export function FoodDialog({openFood, setOpenFood, setOrders, orders}){
         <DialogBanner img={openFood.img}>
           <DialogBannerName>{openFood.name}</DialogBannerName>
         </DialogBanner>
-        <DialogContent/>
+        <DialogContent>
+          <QuantityInput quantity={quantity}/>
+        </DialogContent>
         <DialogFooter>
           <ConfirmButton onClick={addToOrder}>
-            Add to order: {formatPrice(openFood.price)}
+            Add to order: {formatPrice(getPrice(order))}
           </ConfirmButton>
         </DialogFooter>
       </Dialog>
     </>
     ) : null
   ); 
+}
+
+export function FoodDialog(props){
+  if(!props.openFood) return null;
+  return <FoodDialogContainer {...props}/>
 }
