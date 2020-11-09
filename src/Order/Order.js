2e9b729c433e1831d61e4cdfa1a433c5833c1015
HansMoura20 <hansadrienmoura@outlook.com>
2020-11-09 13:58:38 +0800

    Add toppings

--- src/Order/Order.js
+++ src/Order/Order.js
@@ -34,6 +34,10 @@ const OrderItem = styled.div`
   justify-content: space-between;
 `;
 
+const DetailItem = styled.div`
+  color: gray;
+  font-size: 10px;
+`;
 
 export function Order({orders}){
   const subtotal = orders.reduce((total, order) => {
@@ -58,6 +62,14 @@ export function Order({orders}){
                 <div/>
                 <div>{formatPrice(getPrice(order))}</div>
               </OrderItem>
+              <DetailItem>
+                {
+                  order.toppings
+                  .filter(t => t.checked)
+                  .map(topping => topping.name)
+                  .join(", ")
+                }
+              </DetailItem>
             </OrderContainer>
           ))}
           <OrderContainer>
